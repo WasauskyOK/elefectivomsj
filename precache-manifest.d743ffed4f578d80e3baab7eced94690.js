ecdefd021fdc0c76db9c7c7e5762864820808bf4
WasauskyOK <daniechoque159@gmail.com>
2019-12-02 10:06:59 -0500

    Updates

---
+++ precache-manifest.d743ffed4f578d80e3baab7eced94690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab14e73fa74cb85e04148b6f17aa6e5",
+    "url": "/elefectivomsj/index.html"
+  },
+  {
+    "revision": "7da2593901f4cca3661e",
+    "url": "/elefectivomsj/static/css/main.ceacace1.chunk.css"
+  },
+  {
+    "revision": "bce89e1cd74d1e22285a",
+    "url": "/elefectivomsj/static/js/2.a68560bc.chunk.js"
+  },
+  {
+    "revision": "7da2593901f4cca3661e",
+    "url": "/elefectivomsj/static/js/main.952a735a.chunk.js"
+  },
+  {
+    "revision": "e31e1a04a6e35d822a55",
+    "url": "/elefectivomsj/static/js/runtime-main.cdfec024.js"
+  },
+  {
+    "revision": "0f93fa9cdbd888cb551b8c446631ac73",
+    "url": "/elefectivomsj/static/media/WhatsApp Mensaje Recibido.0f93fa9c.mp3"
+  }
+]);
